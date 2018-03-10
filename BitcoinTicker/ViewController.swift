55fd0bdd3eb87b6467341c2130222f24e9c0a18f
Matt Deuschle <mdeuschle@gmail.com>
2018-03-10 10:23:59 -0800

    final project

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -8,78 +8,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let currencySymbol = ["$", "R$", "$", "¥", "€", "£", "$", "Rp", "₪", "₹", "¥", "$", "kr", "$", "zł", "lei", "₽", "kr", "$", "$", "R"]
     var finalURL = ""
+    var currentRow = 0
+    typealias JSON = [String: Any]
 
-    //Pre-setup IBOutlets
     @IBOutlet weak var bitcoinPriceLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
-    
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
     }
 
-    
-    //TODO: Place your 3 UIPickerView delegate methods here
-    
-    
-    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
 
-    
-    
-    
-//    
-//    //MARK: - Networking
-//    /***************************************************************/
-//    
-//    func getWeatherData(url: String, parameters: [String : String]) {
-//        
-//        Alamofire.request(url, method: .get, parameters: parameters)
-//            .responseJSON { response in
-//                if response.result.isSuccess {
-//
-//                    print("Sucess! Got the weather data")
-//                    let weatherJSON : JSON = JSON(response.result.value!)
-//
-//                    self.updateWeatherData(json: weatherJSON)
-//
-//                } else {
-//                    print("Error: \(String(describing: response.result.error))")
-//                    self.bitcoinPriceLabel.text = "Connection Issues"
-//                }
-//            }
-//
-//    }
-//
-//    
-//    
-//    
-//    
-//    //MARK: - JSON Parsing
-//    /***************************************************************/
-//    
-//    func updateWeatherData(json : JSON) {
-//        
-//        if let tempResult = json["main"]["temp"].double {
-//        
-//        weatherData.temperature = Int(round(tempResult!) - 273.15)
-//        weatherData.city = json["name"].stringValue
-//        weatherData.condition = json["weather"][0]["id"].intValue
-//        weatherData.weatherIconName =    weatherData.updateWeatherIcon(condition: weatherData.condition)
-//        }
-//        
-//        updateUIWithWeatherData()
-//    }
-//    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
 
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
 
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        currentRow = row
+        finalURL = baseURL + currencyArray[row]
+        getBitcoinData(url: finalURL)
+    }
 
+    //MARK: - Networking
 
+    private func getBitcoinData(url: String) {
+        guard let url = URL(string: url) else {
+            bitcoinPriceLabel.text = "URL ERROR"
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if data != nil && error == nil {
+                if let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? JSON {
+                    self.updateBitcoinData(json: json!)
+                } else {
+                    self.bitcoinPriceLabel.text = "JSON ERROR"
+                }
+            } else {
+                self.bitcoinPriceLabel.text = "JSON ERROR"
+            }
+        }
+        task.resume()
+    }
+
+    //MARK: - Parse Data
+
+    private func updateBitcoinData(json: JSON) {
+        DispatchQueue.main.async {
+            if let priceDouble = json["last"] as? Double {
+                self.bitcoinPriceLabel.text = self.currencySymbol[self.currentRow] + String(Int(priceDouble))
+            }
+        }
+    }
 }
 
